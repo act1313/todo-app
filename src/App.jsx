bc73f9802edf292d4dde752961a5ca96c1d76718
Aaron <aaronisawesome025@gmail.com>
2023-07-10 14:50:53 -0400

    started tutorial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+
+export default function App() {
+	return <form className="new-item-form">
+		<div className="form-row">
+			<label>New Item</label>
+			<input type="text" id="item" />
+		</div>
+	</form>;
+}
